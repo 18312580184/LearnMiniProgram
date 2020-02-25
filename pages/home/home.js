1470f1dfcb2edfc7155ce87f6c2969eb845544cd
Zard1996 <z18312580184@163.com>
2020-02-26 01:15:28 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,7 @@
 // pages/home/home.js
+
+import request from '../../service/network.js'
+
 Page({
 
     /**
@@ -11,56 +14,25 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
+        request({
+            url: 'http://123.207.32.32:8000/api/hy/recommend'
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
 
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+        // 发送网络请求
+        // wx.request({
+        //     url: 'http://123.207.32.32:8000/api/hy/recommend',
+        //     data: {
+        //         type: 'sell',
+        //         page: 1
+        //     },
+        //     success(res) {
+        //         console.log(res)
+        //     }
+        // })
     }
 })
