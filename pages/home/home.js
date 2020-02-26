db28802d93f18820a6b918133772b1a8c2c8587f
Zard1996 <z18312580184@163.com>
2020-02-26 12:19:47 +0800

    弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,53 @@
 // pages/home/home.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
+    handleShowToast() {
+        wx.showToast({
+            title: 'loading...',
+            duration: 3000,
+            icon: 'loading',
+            mask: true
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    handleShowModal() {
+        wx.showModal({
+            title: 'title',
+            content: 'content',
+            showCancel: false, // 去掉取消按钮
+            success(res) {
+                console.log(res)
+                if (res.cancel) {
+                    console.log('用户点击了取消按钮')
+                }
+                if (res.confirm) {
+                    console.log('用户点击了成功按钮')
+                }
+            }
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    handleShowLoading() {
+        wx.showLoading({
+            title: 'Loading',
+            mask: true //过程不能点击其他地方
+        })
+        setTimeout(() => {
+            wx.hideLoading()
+        }, 1000)
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
+    handleShowAction() {
+        wx.showActionSheet({
+            itemList: ['相册', '拍照'],
+            itemColor: 'red',
+            success(res) {
+                // 获取到点击的是哪一个
+                console.log(res)
+            }
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    onShareAppMessage() {
+        return {
+            title: 'title',
+            path: '/pages/about/about',
+            imageUrl: ''
+        }
     }
 })
